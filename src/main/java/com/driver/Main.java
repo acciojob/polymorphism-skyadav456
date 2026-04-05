e12dcb7c5d543893560366b91ea985dd1c02915e
skyadav456 <sharadk4545@gmail.com>
2026-04-06 00:02:07 +0530

    Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,39 @@
 package com.driver;
-
 public class Main {
 
+
+    static class Product {
+
+        // Task 3
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        // Task 4 (Overloaded)
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        // Task 5 (Overloaded)
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //  Task 2: Create object
+        Product p = new Product();
+
+        // Calling all methods
+
+        int result1 = p.product(2, 3);
+        System.out.println("Product of 2 numbers: " + result1);
+
+        int result2 = p.product(2, 3, 4);
+        System.out.println("Product of 3 numbers: " + result2);
+
+        double result3 = p.product(2.5, 3.5);
+        System.out.println("Product of double numbers: " + result3);
+    }
 }
